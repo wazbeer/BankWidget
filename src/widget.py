36e9e6433831e634a3aae81533dcd8317854713b
wazbeer <ilya1702d@gmail.com>
2026-09-14 11:17:41 +0300

    feat: fix get_date format, add docstring

--- src/widget.py
+++ src/widget.py
@@ -4,6 +4,7 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(data: str) -> str:
+    """Функция принимает счёт либо карту и возвращает комбинрованное значение имени и счёта"""
     parts = data.split()
     num = parts[-1]
     name = " ".join(parts[:-1])
@@ -14,8 +15,9 @@ def mask_account_card(data: str) -> str:
 
 
 def get_date(date_string: str) -> str:
+    """Функция принимает ISO формат времени и возвращет форматированную дату"""
     date_obj = datetime.fromisoformat(date_string)
-    parsed_date = datetime.strftime(date_obj, "%Y.%m.%d")
+    parsed_date = datetime.strftime(date_obj, "%d.%m.%Y")
     return parsed_date
 
 
